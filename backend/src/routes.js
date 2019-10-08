415d5e8359277ed9846357ed9c58cc725c850cb6
Wanderson David <wandersondavid.ifg@gmail.com>
2019-10-07 22:19:51 -0300

    Tela de login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const SessionCrontrolle = require('./controllers/SessionControlle')
+const SessionCrontrolle = require('./controllers/SessionControlle');
+const multer = require('multer')
+const uploadConfig= require('./config/upload')
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardControlle');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
+routes.post('/sessions', SessionCrontrolle.store);
 
-routes.post('/sessions', SessionCrontrolle.store)
-   
+routes.get('/spots',SpotController.index);
+routes.post('/spots', upload.single('thumbnail'),SpotController.store);
+routes.get('/dashboard',DashboardController.show);
 
-module.exports = routes
+routes.post('spots/:spot_id/bookings', BookingController.store) ;
+
+module.exports = routes;
